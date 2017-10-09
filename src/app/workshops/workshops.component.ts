5343c8ff0a424b7dbf9ee3b866d9539a10f979da
EduCFernandes <eduardo.cfernandes96@gmail.com>
2017-10-09 00:18:53 -0300

    Workshops
    Popular array de workshops, ajuste card, background (footer, sobre e contato).

--- src/app/workshops/workshops.component.ts
+++ src/app/workshops/workshops.component.ts
@@ -10,53 +10,61 @@ import {Workshops} from './lista-workshops/workshop.model';
 export class WorkshopsComponent implements OnInit {
   workshops: Workshops[] = [
      {
-       id:"workshop-swift",
-       nome:"Desenvolvimento com Swift",
-       data: "08/10/2017",
-       vagas:"heue",
+       id:"workshop-flat-design",
+       nome:"Flat Design",
+       data: "11/12/2017",
+       vagas:"22",
        local: "São Paulo",
-       image: "assets/img/curso/curso1.png"
+       image: "assets/img/workshops/workshop1.jpg"
      },
      {
-       id:"workshop-swift",
-       nome:"Desenvolvimento com Swift",
-       data: "08/10/2017",
-       vagas:"heue",
+       id:"workshop-tasl-runner",
+       nome:"Task Runners",
+       data: "1/10/2017",
+       vagas:"15",
        local: "São Paulo",
-       image: "assets/img/curso/curso1.png"
+       image: "assets/img/workshops/workshop2.jpg"
      },
      {
-       id:"workshop-swift",
-       nome:"Desenvolvimento com Swift",
-       data: "08/10/2017",
-       vagas:"heue",
+       id:"workshop-photoshop",
+       nome:"Photoshop",
+       data: "15/10/2017",
+       vagas:"20",
        local: "São Paulo",
-       image: "assets/img/curso/curso1.png"
+       image: "assets/img/workshops/workshop3.png"
      },
      {
-       id:"workshop-swift",
-       nome:"Desenvolvimento com Swift",
-       data: "08/10/2017",
-       vagas:"heue",
-       local: "São Paulo",
-       image: "assets/img/curso/curso1.png"
+       id:"workshop-midias-sociais",
+       nome:"Midias Sociais",
+       data: "16/10/2017",
+       vagas:"10",
+       local: "Rio de Janeiro",
+       image: "assets/img/workshops/workshop4.png"
      },
      {
-       id:"workshop-swift",
-       nome:"Desenvolvimento com Swift",
-       data: "08/10/2017",
-       vagas:"heue",
+       id:"workshop-angular",
+       nome:"Angular",
+       data: "02/11/2017",
+       vagas:"25",
        local: "São Paulo",
-       image: "assets/img/curso/curso1.png"
+       image: "assets/img/workshops/workshop5.png"
      },
      {
-       id:"workshop-swift",
-       nome:"Desenvolvimento com Swift",
-       data: "08/10/2017",
-       vagas:"heue",
+       id:"workshop-edicao-video",
+       nome:"Edição de Vídeo",
+       data: "24/12/2017",
+       vagas:"20",
        local: "São Paulo",
-       image: "assets/img/curso/curso1.png"
-     }
+       image: "assets/img/workshops/workshop6.png"
+     },
+     {
+       id:"workshop-marketing-digital",
+       nome:"Marketing Digital",
+       data: "18/10/2017",
+       vagas:"10",
+       local: "São Paulo",
+       image: "assets/img/workshops/workshop7.jpg"
+     },
   ]
 
 
